1c8e31188369c1902e9e5a1ed5a8b4d439815f77
Curtis Pritchard <curtis.pritchard@stclairconnect.ca>
2020-03-11 16:31:30 -0400

    #40 #4 #39 #38 added template items * Added profile placeholder * Added exchange tab * Added border wait time * Added map leading user to one location * User can move through multiple pages from the bottom nav

---
+++ lib/HomePage.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+          children: <Widget>[
+            buildExchangeBar(),
+            Row(children: <Widget>[
+              Expanded(child: Align(child: Text("Border Wait Time"), alignment: Alignment.centerLeft,),),
+              Expanded(child: Align(child: DropDown(), alignment: Alignment.centerRight,),),
+            ],),
+            WaitTime(),
+            Row(children: <Widget>[
+              Text("Traffic")
+            ],),
+            GoogleMaps()
+
+          ],
+    );
+  }
+}
+
+Container buildExchangeBar() {
+    int mun = 100;
+    return Container(
+              color: Colors.yellow,
+              height: 30,
+              child: Row(
+                children: <Widget>[
+                  Expanded( child: Align(child: Text("Exchange Rate:"), 
+                  alignment: Alignment.centerLeft,)),
+                  Expanded( child: Align(child: Text("\$${mun}"), 
+                  alignment: Alignment.centerRight,))
+                ],
+              )
+            );
+  }
+
+
+class DropDown extends StatefulWidget {
+  @override
+  _DropDownState createState() => _DropDownState();
+}
+
+class _DropDownState extends State<DropDown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+
+class WaitTime extends StatefulWidget {
+  
+  @override
+  _WaitTimeState createState() => _WaitTimeState();
+}
+
+class _WaitTimeState extends State<WaitTime> {
+  int _waitTimeDiff = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+                children: <Widget>[
+                  Expanded(child: 
+                  (_waitTimeDiff > 0) ? Container(
+                    color: Colors.green,
+                    height: 250,
+                  ) : Container(
+                    color: Colors.red,
+                    height: 250,
+                  ),
+                  ),
+                  
+                  Expanded(child: 
+                  (_waitTimeDiff < 0) ? Container(
+                    color: Colors.green,
+                    height: 250,
+                  ) : Container(
+                    color: Colors.red,
+                    height: 250,
+                  ),
+                  ),
+                ],
+              );
+  }
+}
+
+class GoogleMaps extends StatefulWidget {
+  @override
+  _GoogleMapsState createState() => _GoogleMapsState();
+}
+
+class _GoogleMapsState extends State<GoogleMaps> {
+ GoogleMapController gmc;
+
+
+ void _onMapCreated(GoogleMapController controller) {
+    gmc = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(42.311180,-82.859060),
+            zoom: 15
+          ),
+          mapType: MapType.hybrid,
+        ),
+    );
+  }
+}
